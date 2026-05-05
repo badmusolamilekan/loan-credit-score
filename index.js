465e0e4aac9f37ea73d01fe78a5f794a9daf90cc
Badmus Olamilekan <badmusolamilekan93@gmail.com>
2026-05-05 10:11:09 +0100

    loan validation with credit score

--- index.js
+++ index.js
@@ -15,22 +15,29 @@ loanForm.addEventListener("submit", function (e) {
   ) {
     messages.innerHTML = "Please Enter your credit Score and your Loan amount.";
     messages.classList.add("text-red-500");
-  } else if (creditScore >= 0 && creditScore <= 3) {
+  } else if (creditScore === 0 && creditScore <= 3) {
     messages.innerHTML = "Sorry! you are not eligible to borrow money";
     messages.classList.add("text-red-500");
   } else if (creditScore >= 4 && creditScore <= 7) {
-    if (loanAmount <= 5000) {
+    if (loanAmount < 1) {
+      messages.innerHTML =
+        "You are eligible to borrow but you cannot borrow below 1.";
+      messages.classList.add("text-red-500");
+    } else if (loanAmount <= 5000) {
       messages.innerHTML =
         "You are eligible to borrow 5,000 and your loan has been successfully granted.";
       messages.classList.add("text-green-500");
-      ("grren");
     } else {
       messages.innerHTML =
         "You are eligible to borrow but you cannot borrow more than 5,000.";
       messages.classList.add("text-red-500");
     }
   } else if (creditScore >= 8 && creditScore <= 9) {
-    if (loanAmount <= 10000) {
+    if (loanAmount < 1) {
+      messages.innerHTML =
+        "You are eligible to borrow but you cannot borrow below 1.";
+      messages.classList.add("text-red-500");
+    } else if (loanAmount <= 10000) {
       messages.innerHTML =
         "You are eligible to borrow 10,000 and your loan has been successfully granted.";
       messages.classList.add("text-green-500");
@@ -40,8 +47,14 @@ loanForm.addEventListener("submit", function (e) {
       messages.classList.add("text-red-500");
     }
   } else if (creditScore === 10) {
-    messages.innerHTML = "Your Loan has been granted Successfully";
-    messages.classList.add("text-green-500");
+    if (loanAmount < 1) {
+      messages.innerHTML =
+        "You are eligible to borrow but you cannot borrow below 1.";
+      messages.classList.add("text-red-500");
+    } else {
+      messages.innerHTML = "Your Loan has been granted Successfully";
+      messages.classList.add("text-green-500");
+    }
   } else {
     messages.innerHTML = "Make sure you enter between 0 - 10";
     messages.classList.add("text-red-500");
